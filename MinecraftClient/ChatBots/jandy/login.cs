1bbc4799cfbc87e605e4bf3b592f1a9548cba150
bilibilijandy <141329748+bilibilijandy@users.noreply.github.com>
2025-01-01 14:30:59 +0800

    1

--- MinecraftClient/ChatBots/jandy/login.cs
+++ MinecraftClient/ChatBots/jandy/login.cs
@@ -15,9 +15,6 @@ public class JLoginBot:ChatBot
     private JLoginBotConfig _config = new JLoginBotConfig();
     private bool _enteredLock;
 
-    private string result;
-    private string mm;
-
     public override void Initialize()
     {
         _enteredLock = false;
@@ -25,7 +22,7 @@ public class JLoginBot:ChatBot
         {
             LogToConsole("没有找到配置文件，正在释放....");
             string yaml = YamlHelper.Serialize(_config);
-            yaml = "# XotBot @ XTeam\n# 请参阅用户手册来了解如何使用此处\n"+yaml;
+            //yaml = yaml;
             File.WriteAllText("./JLoginConf.yaml", yaml);
         }
 
@@ -43,33 +40,7 @@ public class JLoginBot:ChatBot
             return;
         }
 
-
-        SendText($"/login {mm}");
-        LogToConsole($"登录成功,当前密码 {mm} ");
-
-        //随机密码功能
-        var random = new Random();
-        string filePath = "mm.txt";
-        string oldMM = "oldmm.txt";
-        result = GenerateRandomString(random);//新密码
-
-        mm = File.ReadAllText(filePath); 
-        LogToConsole($"{mm},{result}");
-
-        // 将字符串写入文件
-        using (StreamWriter writer = new StreamWriter(filePath, false)) // true表示追加模式
-        {
-            writer.WriteLine(result);
-        }
-
-        using (StreamWriter writer = new StreamWriter(oldMM, true)) // true表示追加模式
-        {
-            writer.WriteLine($"{result}\n");
-        }
-
-        Thread.Sleep(GetSleepTime());
-        SendText($"/cp {mm} {result}");
-        LogToConsole($"修改密码成功,当前密码 {result} ,原密码 {mm} ");
+        SendText($"/login {_config.DefaultPasswd}");
         Thread.Sleep(GetSleepTime());
         ChangeSlot(2);
         LogToConsole("切换物品成功");
@@ -96,43 +67,14 @@ public class JLoginBot:ChatBot
         return res;
     }
 
-    static string GenerateRandomString(Random random)
-    {
-        // 确保 'e', 'd', 'e', 'n' 顺序
-        List<char> chars = new List<char> { 'e', 'd', 'e', 'n', 'n', 'b', '6', '6', '6' };
-
-        // 随机打乱除 'e', 'd', 'e', 'n' 之外的字符
-        var shuffleChars = chars.Skip(4).ToList();
-        shuffleChars = shuffleChars.OrderBy(a => random.Next()).ToList();
-
-        // 重新组合字符串
-        string shuffled = new string(chars.Take(4).Concat(shuffleChars).ToArray());
-
-        // 插入随机数量的下划线
-        int maxUnderscore = 8; // 根据字符数量设置最大下划线数
-        int underscoreCount = random.Next(maxUnderscore + 1);
-
-        for (int i = 0; i < underscoreCount; i++)
-        {
-            int insertIndex = random.Next(shuffled.Length + 1);
-            shuffled = shuffled.Insert(insertIndex, "_");
-        }
-
-        // 检查是否包含 'eden'，如果不包含，重新生成
-        if (!shuffled.Contains("eden"))
-        {
-            return GenerateRandomString(random); // 递归调用以确保包含 'eden'
-        }
-
-        return shuffled;
-    }
+    
 }
 
 [Serializable]
 public class JLoginBotConfig
 {
     [YamlMember(Alias = "defaultPasswd", ApplyNamingConventions = false)]
-    public string DefaultPasswd = "#XBotDefaultPasswd123";
+    public string DefaultPasswd = "#JBotDefaultPasswd123";
     [YamlMember(Alias = "reconnectTimeout", ApplyNamingConventions = false)]
     public float ReconnectTimeout = 3.1f;
 
--- MinecraftClient/ChatBots/jandy/msg.cs
+++ MinecraftClient/ChatBots/jandy/msg.cs
@@ -24,7 +24,6 @@ public class JMsgBot:ChatBot
         {
             LogToConsole("没有找到配置文件，正在释放....");
             string yaml = YamlHelper.Serialize(_config);
-            yaml = "# XotBot @ XTeam\n# 请参阅用户手册来了解如何使用此处\n"+yaml;
             File.WriteAllText("./JMsgBotConf.yaml", yaml);
         }
 
@@ -84,7 +83,7 @@ public class JMsgBotConfig {
 
     [YamlMember(Alias = "messages", ApplyNamingConventions = false)]
     public string[] Messages =
-        { "默认测试宣传文本 XTeam @ XotBot", "测试宣传文本2 XTeam @ XotBot", "测试宣传文本3 XTeam @ XotBot" };
+        { "默认测试宣传文本", "测试宣传文本2", "测试宣传文本3" };
 
 }
 
