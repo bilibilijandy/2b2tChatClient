42389cc75affd5d9fd808ee4c0abca5f3c1a0831
bilibilijandy <320545809@qq.com>
2025-01-04 11:21:05 +0800

    创建 command.cs

---
+++ MinecraftClient/ChatBots/jandy/command.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic; // 添加此行
+using System.Linq; // 添加此行
+using MinecraftClient.Scripting;
+using YamlDotNet.Serialization;
+using MinecraftClient.Commands;
+using System.Text.RegularExpressions;
+
+namespace MinecraftClient.ChatBots.JandyBot;
+
+public class JCommandBot:ChatBot
+{
+
+    private float _ableTime = 5;
+    private int _count = 0;
+    private int _player = 1;
+    private List<string> age = new List<string>();
+    public override void Initialize()
+    {
+        
+
+    }
+
+    public override void AfterGameJoined()
+    {
+        
+    }
+
+    public override void GetText(string text)
+        {
+            text = GetVerbatim(text);
+            string sender = "";
+            string message = "";
+
+            if (IsChatMessage(text, ref message, ref sender))
+            {
+                Match match = Regex.Match(message, @"[!！](.*)");
+                if (match.Success)
+                {
+                    string comm = match.Groups[1].Value;
+                    string parameters = match.Groups[2].Value;
+                    age = parameters.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                }
+            }
+            else if (IsPrivateMessage(text, ref message, ref sender))
+            {
+
+            }
+        }
+
+    
+
+    public void commands(string command, List<string> parameters)
+    {
+        switch (command)
+        {
+            case "help":
+                SendText("帮助");
+                break;
+            case "echo":
+                SendText(parameters[0]);
+                break;
+            case "stat":
+                SendText("当前服务器版本:1.20.4；客户端版本:1.20.4");
+                break;
+        }
+    }
+}
+
