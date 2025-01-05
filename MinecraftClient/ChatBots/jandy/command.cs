3fd6690a39550a2cbd4ec0dacd5162e695c8f1c9
bilibilijandy <320545809@qq.com>
2025-01-05 13:58:24 +0800

    feat(ChatBots): 更新 JCommandBot 功能
    - 添加管理员列表命令
    - 优化帮助信息和状态信息的显示
    - 增加文本清理功能，去除颜色代码等特殊字符
    - 改进命令匹配逻辑

--- MinecraftClient/ChatBots/jandy/command.cs
+++ MinecraftClient/ChatBots/jandy/command.cs
@@ -29,8 +29,9 @@ public class JCommandBot : ChatBot
 
     public override void GetText(string text)
     {
-        Match match = Regex.Match(text, @"[^\w]*[!！](\w+)\s*(.*)");
-            if (match.Success)
+        text=GetVerbatim(text);//去除颜色代码
+        Match match = Regex.Match(text, @"[^\w]*[!！](\w+)\s*(.*)");//匹配命令
+        if (match.Success)//匹配成功
             {
                 string comm = match.Groups[1].Value;
                 string parameters = match.Groups[2].Value;
@@ -41,16 +42,22 @@ public class JCommandBot : ChatBot
 
     public void commands(string command, List<string> parameters)
     {
+        /**
+         * 命令列表
+         * help 帮助
+         * echo 回显
+         * stat 状态
+         */
         LogToConsole("command: " + command);
         switch (command)
         {
             case "help":
-                SendText("帮助");
+                SendText(cleanText("帮助"));
                 break;
             case "echo":
                 if (parameters.Count > 0)
                 {
-                    SendText(parameters[0]);
+                    SendText(cleanText(parameters[0]));
                 }
                 else
                 {
@@ -58,11 +65,20 @@ public class JCommandBot : ChatBot
                 }
                 break;
             case "stat":
-                SendText("当前服务器版本:1.20.4；客户端版本:1.20.4,已加载模块：loginv1.0;msgv1.0;commandv1.1。");
+                SendText(cleanText("当前服务器版本:1.20.4；客户端版本:1.20.4,已加载模块：loginv1.0;msgv1.0;commandv1.5。"));
                 break;
-            default:
-                SendText("未知命令");
+            case "admin":
+                SendText(cleanText("管理员列表：bilibili_jandy;jandy的2b2t;jandy的2b2t号"));
                 break;
         }
     }
+
+    public string cleanText(string text)
+    {
+        text = text.Replace("§", "");
+        text = text.Replace("/", "");
+        text = GetVerbatim(text);
+        return text;
+    }
+
 }
